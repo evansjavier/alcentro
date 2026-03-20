0dc933e9baae1704fd6cfb2908f48a59ae37697c
Javier Evans <evansjavier16@gmail.com>
2026-03-20 16:27:20 -0400

    refactor: usar dropdown para acciones de pagos

--- resources/views/livewire/payments/index.blade.php
+++ resources/views/livewire/payments/index.blade.php
@@ -89,13 +89,41 @@
                                     </span>
                                 </td>
                                 <td class="px-4 py-3 text-right">
-                                    <div class="flex items-center justify-end gap-2">
-                                        @if(!$payment->is_approved)
-                                            <a class="kt-btn kt-btn-light kt-btn-sm" href="{{ route("payments.edit", $payment) }}">Editar</a>
-                                        @else
-                                            <a class="kt-btn kt-btn-light kt-btn-sm" href="{{ route("payments.show", $payment) }}">Ver</a>
-                                        @endif
-                                        <a class="kt-btn kt-btn-light kt-btn-sm" href="{{ route("invoices.show", $payment->invoice_id) }}">Ver factura</a>
+                                    <div class="kt-menu justify-end" data-kt-menu="true">
+                                        <div class="kt-menu-item kt-menu-item-dropdown" data-kt-menu-item-offset="0, 10px" data-kt-menu-item-placement="bottom-end" data-kt-menu-item-toggle="dropdown" data-kt-menu-item-trigger="click">
+                                            <button class="kt-menu-toggle kt-btn kt-btn-sm kt-btn-icon kt-btn-ghost">
+                                                <i class="ki-filled ki-dots-vertical text-lg"></i>
+                                            </button>
+                                            <div class="kt-menu-dropdown kt-menu-default w-full max-w-[175px] text-left" data-kt-menu-dismiss="true">
+                                                @if(!$payment->is_approved)
+                                                    <div class="kt-menu-item">
+                                                        <a class="kt-menu-link" href="{{ route('payments.edit', $payment) }}">
+                                                            <span class="kt-menu-icon">
+                                                                <i class="ki-filled ki-pencil"></i>
+                                                            </span>
+                                                            <span class="kt-menu-title">Editar Pago</span>
+                                                        </a>
+                                                    </div>
+                                                @endif
+                                                <div class="kt-menu-item">
+                                                    <a class="kt-menu-link" href="{{ route('payments.show', $payment) }}">
+                                                        <span class="kt-menu-icon">
+                                                            <i class="ki-filled ki-search-list"></i>
+                                                        </span>
+                                                        <span class="kt-menu-title">Ver Detalles</span>
+                                                    </a>
+                                                </div>
+                                                <div class="kt-menu-separator"></div>
+                                                <div class="kt-menu-item">
+                                                    <a class="kt-menu-link" href="{{ route('invoices.show', $payment->invoice_id) }}">
+                                                        <span class="kt-menu-icon">
+                                                            <i class="ki-filled ki-bill"></i>
+                                                        </span>
+                                                        <span class="kt-menu-title">Ver Factura</span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </td>
                             </tr>
